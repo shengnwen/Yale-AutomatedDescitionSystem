0d853951b0b15e3caf1f844c8159152eb38cb77b
shengnwen <shengwen@live.cn>
2015-10-29 15:25:35 -0400

    ads hw3 foundation

---
+++ assignment3/hw3.py
@@ -0,0 +1,11 @@
+__author__ = 'shengwen'
+
+
+def backtest(tiker = "HD", start = "2006-10-01", end="2015-10-01", duration = 50):
+    return 0.01
+
+def sectortest(periods, durations):
+    return
+
+def realbacktest(tiker = "HD", start = "2006-10-01", end="2015-10-01", duration = 20, basis=2, file="Path"):
+    return

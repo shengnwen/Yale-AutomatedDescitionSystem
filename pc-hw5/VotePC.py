55b6d9bb8343911265bbbbe391408689c5969ee3
shengnwen <shengwen@live.cn>
2015-12-20 23:45:25 -0500

    start final project

--- pc-hw5/VotePC.py
+++ pc-hw5/VotePC.py
@@ -1,3 +1,5 @@
+from Tkinter import Tk, Frame, BOTH
+
 class Person:
     # the person has a set of preferences,
     # a collection of relatinships, and
@@ -13,8 +15,38 @@ class Person:
     def __str__(self):
         pass
 
+class PC:
+    def __init__(self):
+        pass
+    def __str__(self):
+        pass
+
 def whichpc(person):
     # price, memory size, screen size, disk space, etc.
     #user classification system that accounts for
     # why a person actually needs a computer.
     pass
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="white")
+
+        self.parent = parent
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.parent.title("Simple")
+        self.pack(fill=BOTH, expand=1)
+
+def main():
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = Example(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
